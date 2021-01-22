ebf8cd097c9f553fdd90aa8fec0cd118252589ef
lukevangorder <lukevangorder@gmail.com>
2021-01-22 14:48:54 -0500

    commit tree e2f60ff6593e6b87e85d2c0f1c25af21d003bca4

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+    attr_accessor :roster
+    attr_reader :name
+    def initialize(name)
+        @name=name
+        @roster = {}
+    end
+    def add_student(name, grade)
+        if @roster.keys.include?(grade)
+            @roster[grade].push(name)
+        else
+            @roster[grade] = []
+            @roster[grade].push(name)
+        end
+    end
+    def grade(num)
+        @roster[num]
+    end
+    def sort
+        @roster.each do |grade, students|
+            @roster[grade] = students.sort
+        end
+    end
+end
